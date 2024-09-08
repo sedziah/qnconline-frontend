422760d823dac7034c0462324dce04b23422bef5
Dale Boahene <dhalehk@gmail.com>
2024-09-08 18:21:29 +0000

    chore: changed favicon

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,12 @@
 import { ProductListingPage } from '@/components'
+import { Metadata } from 'next'
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: "QNCONLINE - Products",
+  description: "A Home of Premium Products & Exemplary Customer Care!",
+}
+
 
 const ProductListing = () => {
   return <ProductListingPage />
