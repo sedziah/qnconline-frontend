f5f61ac5effef9d9d3d13578657fc4b177fd9536
Dale Boahene <dhalehk@gmail.com>
2024-09-08 13:32:04 +0000

    chore: removed all dark styles

--- components/Cards/InfoCard.tsx
+++ components/Cards/InfoCard.tsx
@@ -19,7 +19,7 @@ const InfoCard = ({title, description, link }: CardProp) => {
           <p className="text-slate-700 mt-3 max-w-lg">{description}</p>
 
           <Link href={link}>
-          <button type="button" className="mt-4 flex items-center justify-center w-full rounded-lg border border-primary px-5 py-2.5 text-xs font-medium text-primary hover:bg-slate-200 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+          <button type="button" className="mt-4 flex items-center justify-center w-full rounded-lg border border-primary px-5 py-2.5 text-xs font-medium text-primary hover:bg-slate-200 focus:outline-none focus:ring-4  focus:ring-primary-300">
             <svg className="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
             </svg>
