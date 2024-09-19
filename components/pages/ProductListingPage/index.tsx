487f1e7410067edac9a988a8c362c05deb520b19
Dale Boahene <dhalehk@gmail.com>
2024-09-19 22:01:08 +0000

    chore: cleanup and seperated flating filters component

--- components/pages/ProductListingPage/index.tsx
+++ components/pages/ProductListingPage/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Breadcrumb from '@/components/breadcrumb';
 import { CTAOne } from '@/components/CTAS/CTAOne';
+import FloatingFilter from '@/components/FloatingFilter'
 import ProductsWrapper from '@/components/ProductsWrapper';
 import { useSearchParams } from 'next/navigation';
 import React, { Suspense } from 'react';  
@@ -38,19 +39,7 @@ const ProductContent = () => {
         <ProductsWrapper categorySlug={categorySlug} categoryName={categoryName} />
       )}
 
-      <div className='block lg:hidden md:hidden bg-white shadow-md transition-all border-t-2 border-lightGray/20 px-4 py-3 w-full fixed bottom-0 '>
-        <div className='flex flex-row items-center justify-center w-full'>
-          <button className='w-full justify-center h-10 flex items-center text-base flex-row gap-x-2'>
-            <p>Filters</p>
-            <FaFilter />
-          </button>
-          <div className='h-8 w-0.5 border bg-lightGray/20'></div>
-          <button className='w-full justify-center h-10 flex items-center text-base flex-row gap-x-2'>
-            <p>Sort</p>
-            <BiSortAlt2 />
-          </button>
-        </div>
-      </div>
+      <FloatingFilter />
     </>
   );
 };
