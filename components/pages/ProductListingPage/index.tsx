30721cc5941ac42af5654adf000d74040bb71759
Dale Boahene <dhalehk@gmail.com>
2024-09-19 22:33:21 +0000

    chore: code cleanup

--- components/pages/ProductListingPage/index.tsx
+++ components/pages/ProductListingPage/index.tsx
@@ -1,33 +1,35 @@
-"use client";
-import Breadcrumb from '@/components/breadcrumb';
-import { CTAOne } from '@/components/CTAS/CTAOne';
+"use client"
+import Breadcrumb from '@/components/breadcrumb'
+import { CTAOne } from '@/components/CTAS/CTAOne'
 import FloatingFilter from '@/components/FloatingFilter'
-import ProductsWrapper from '@/components/ProductsWrapper';
-import { useSearchParams } from 'next/navigation';
-import React, { Suspense } from 'react';  
-import { BiSortAlt2 } from "react-icons/bi";
-import { FaFilter } from "react-icons/fa";
+import ProductsWrapper from '@/components/ProductsWrapper'
+import { useSearchParams } from 'next/navigation'
+import React, { Suspense } from 'react'
+import { BiSortAlt2 } from "react-icons/bi"
+import { FaFilter } from "react-icons/fa"
 
 const ProductListingPage = () => {
   return (
     <>
       {/* Loading indicator while waiting for the content */}
-      <Suspense fallback={<div>Loading -...</div>}>
+      <Suspense fallback={<div><div
+        className="h-40 w-full bg-gray-200 rounded animate-pulse"
+      ></div></div>}>
         <ProductContent />
       </Suspense>
     </>
-  );
-};
+  )
+}
 
 const ProductContent = () => {
-  const searchParams = useSearchParams();
-  const categorySlug = searchParams.get('s'); // Extract the slug from the URL
-  const categoryName = searchParams.get('name') || 'All Products'; // Extract the name from the URL
+  const searchParams = useSearchParams()
+  const categorySlug = searchParams.get('s') // Extract the slug from the URL
+  const categoryName = searchParams.get('name') || 'All Products' // Extract the name from the URL
 
   const breadcrumbItems = [
     { label: 'Home', href: '/' },
     { label: categoryName },  // Display the category name in the breadcrumb
-  ];
+  ]
 
   return (
     <>
@@ -41,7 +43,7 @@ const ProductContent = () => {
 
       <FloatingFilter />
     </>
-  );
-};
+  )
+}
 
-export default ProductListingPage;
+export default ProductListingPage
