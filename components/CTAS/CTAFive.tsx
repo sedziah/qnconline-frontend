3f188d69d2f917da9fa5bed1ee180498751102c3
Raymond Apungu <apunguray@gmail.com>
2024-10-09 09:30:31 +0000

    chore: update layout

--- components/CTAS/CTAFive.tsx
+++ components/CTAS/CTAFive.tsx
@@ -14,7 +14,7 @@ const CTAFive = () => {
       </div>
 
       <div className="mt-10 w-full p-3 bg-white rounded-md">
-        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           <div className='flex flex-row items-center gap-x-3'>
             <div className="w-10 h-10 flex items-center justify-center bg-bglight rounded-md">
               <Verify size="20" color="#374151" />
