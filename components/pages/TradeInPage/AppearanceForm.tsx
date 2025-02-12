4e4e5a5205aff2b00982fadbdaa129e9df3580dc
Solomon <sedziah1@gmail.com>
2025-02-12 17:30:41 +0000

    🐞 fix: trade in pages

--- components/pages/TradeInPage/AppearanceForm.tsx
+++ components/pages/TradeInPage/AppearanceForm.tsx
@@ -2,7 +2,7 @@ import { ANIMATIONSTYLE } from "@/constants";
 import React, { useState } from "react";
 
 type Props = {
-  handleNext: () => void;
+  handleNext: (selectedData: { label: string; value: string }) => void;
 };
 
 const conditionOptions = [
@@ -34,16 +34,16 @@ const AppearanceForm = ({ handleNext }: Props) => {
     <div style={{ ...ANIMATIONSTYLE }} className="w-full transition">
       <div className="my-7">
         <h1 className="text-lg font-semibold text-black">
-          How do the sides and back look??
+          How do the sides and back look?
         </h1>
       </div>
       <div className="my-7">
         <div className="space-y-5">
-          {conditionOptions?.map((condition, index) => (
+          {conditionOptions?.map((condition) => (
             <button
-              onClick={() => setSelectedOption(condition?.title)}
               key={condition?.title}
-              className={`w-full transition-opacity my-2  delay-150  gap-x-3 flex flex-row justify-between rounded-md p-5 border  hover:bg-[#bde0fe]/10 hover:border-l[#bde0fe]/40 bg-white`}
+              onClick={() => setSelectedOption(condition?.title)}
+              className={`w-full transition-opacity my-2 gap-x-3 flex flex-row justify-between rounded-md p-5 border hover:bg-[#bde0fe]/10 hover:border-l[#bde0fe]/40 bg-white`}
             >
               {selectedOption === condition?.title ? (
                 <div className="w-5 h-5 border flex flex-row items-center justify-center bg-primary border-primary rounded-full">
@@ -63,7 +63,7 @@ const AppearanceForm = ({ handleNext }: Props) => {
                 >
                   {condition?.title}
                 </p>
-                <p className={`font-normal text-[13px] mt-2 text-gray-500`}>
+                <p className="font-normal text-[13px] mt-2 text-gray-500">
                   {condition?.description}
                 </p>
               </div>
@@ -76,7 +76,12 @@ const AppearanceForm = ({ handleNext }: Props) => {
         className="my-7 transition flex items-end justify-end"
       >
         <button
-          onClick={handleNext}
+          onClick={() => {
+            if (selectedOption) {
+              handleNext({ label: "Appearance", value: selectedOption });
+            }
+          }}
+          disabled={!selectedOption} // Prevent proceeding without selection
           className="h-10 px-5 flex items-center justify-center text-white text-sm font-medium bg-primary rounded-md"
         >
           Next
--- components/pages/TradeInPage/FunctionalityForm.tsx
+++ components/pages/TradeInPage/FunctionalityForm.tsx
@@ -2,8 +2,8 @@ import { ANIMATIONSTYLE } from '@/constants'
 import React, { useState } from 'react'
 
 type Props = {
-  handleNext: () => void
-}
+  handleNext: (selectedData: { label: string; value: string }) => void;
+};
 
 const functionalityQuestions = [
   "The device turns on, turns off, and charges. It has a battery, case, and SIM drawer.",
@@ -12,13 +12,10 @@ const functionalityQuestions = [
   "Touch ID and Face ID are functional (if present).",
   "All other features including Wi-Fi, Bluetooth, buttons, etc. work perfectly.",
   "Important: iCloud, Google, or any other accounts must be disconnected whether your item is functional or not. We don't accept bent and/or oxidized items."
-]
-
-
+];
 
 export const FunctionalityForm = ({ handleNext }: Props) => {
-  const [selectedOption, setSelectedOption] = useState<string | null>(null)
-
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
   return (
     <div style={{ ...ANIMATIONSTYLE }} className='w-full transition'>
@@ -28,43 +25,52 @@ export const FunctionalityForm = ({ handleNext }: Props) => {
         </h1>
 
         <div className="my-7">
-          {functionalityQuestions?.map((question) => <p key={question} className='my-3 text-sm font-normal text-black'>
-            {`• ${question}`}
-          </p>)}
+          {functionalityQuestions?.map((question) => (
+            <p key={question} className='my-3 text-sm font-normal text-black'>
+              {`• ${question}`}
+            </p>
+          ))}
         </div>
 
         <div className='flex flex-row items-center justify-center gap-x-5'>
-          {["No", "Yes"].map((condition, index) => (
+          {["No", "Yes"].map((condition) => (
             <button
-              onClick={() => setSelectedOption(condition)}
               key={condition}
-              className={`w-full transition-opacity my-2  delay-150  gap-x-3 flex flex-row justify-between rounded-md p-5 border  hover:bg-[#bde0fe]/10 hover:border-l[#bde0fe]/40 bg-white`}
+              onClick={() => setSelectedOption(condition)}
+              className={`w-full transition-opacity my-2 gap-x-3 flex flex-row justify-between rounded-md p-5 border hover:bg-[#bde0fe]/10 hover:border-l[#bde0fe]/40 bg-white`}
             >
-
-
-              {selectedOption === condition ? <div className='w-5 h-5 border flex flex-row items-center justify-center bg-primary border-primary rounded-full'>
-                <div className='h-2 w-2 bg-white rounded-full'></div>
-              </div> :
+              {selectedOption === condition ? (
+                <div className='w-5 h-5 border flex flex-row items-center justify-center bg-primary border-primary rounded-full'>
+                  <div className='h-2 w-2 bg-white rounded-full'></div>
+                </div>
+              ) : (
                 <div className='w-5 h-5 border border-primary rounded-full'></div>
-              }
+              )}
 
               <div className='flex-1 flex flex-col items-start'>
                 <p className={`${selectedOption === condition ? "font-bold" : "font-normal"} text-sm text-black`}>
                   {condition}
                 </p>
-
               </div>
-
             </button>
           ))}
         </div>
 
+        {/* See the Offer Button */}
         <div style={{ ...ANIMATIONSTYLE }} className='my-7 transition flex items-end justify-end'>
-          <button onClick={handleNext} className='h-10 px-5 flex items-center justify-center text-white text-sm font-medium bg-primary rounded-md'>
+          <button
+            onClick={() => {
+              if (selectedOption) {
+                handleNext({ label: "Functionality", value: selectedOption });
+              }
+            }}
+            disabled={!selectedOption} // Prevent proceeding without selection
+            className='h-10 px-5 flex items-center justify-center text-white text-sm font-medium bg-primary rounded-md'
+          >
             See the offer
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
